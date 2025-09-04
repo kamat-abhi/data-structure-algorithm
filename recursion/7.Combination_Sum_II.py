55cd612e36acefc8a18f7f344cbe11e4c30c2494
abhishek kamat <abhishekkamat142@gmail.com>
2025-09-04 08:43:50 +0530

    Implement combination sum II solution

---
+++ recursion/7.Combination_Sum_II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum2(self, candidates):
+        result = []
+        candidates.sort()  
+        self.solve(candidates, 0, target, [], result)
+        return result
+    def solve(self, arr, idx, tar, temp, result):
+        if tar == 0:
+            result.append(temp[:])
+            return
+        if tar < 0:
+            return
+        for i in range(idx, len(arr)):
+            if i > idx and arr[i] == arr[i-1]:
+                continue
+            temp.append(arr[i])
+            self.solve(arr, i+1, tar-arr[i], temp, result)
+            temp.pop()
