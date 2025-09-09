3a861fefe2763bbee19493e1e3060e4538b6f320
abhishek kamat <abhishekkamat142@gmail.com>
2025-09-09 09:48:32 +0530

    Fix index errors in N-Queens validity checks and ensure correct board state updates

--- recursion/N-Queens.py
+++ recursion/N-Queens.py
@@ -10,20 +10,20 @@ class Solution:
             #upward check
             i = row-1
             while i>=0:
-                if board[row][col] == "Q":
+                if board[i][col] == "Q":
                     return False
                 i -= 1
             #left diagonal upward check
             i, j = row - 1, col - 1
             while i>=0 and j>=0:
-                if board[row][col] == "Q":
+                if board[i][j] == "Q":
                     return False
                 i -= 1
                 j -= 1
             #right diagonal upward check
             i, j = row - 1, col + 1
             while i>=0 and j<n:
-                if board[row][col] == "Q":
+                if board[i][j] == "Q":
                     return False
                 i -= 1
                 j += 1
@@ -31,13 +31,16 @@ class Solution:
 
         def solve(board, row):
             if row == n:
-                result.append(board)
+                result.append(board[:])
                 return
             for col in range(n):
                 if isValid(board, row, col):
-                    board[row][col] == "Q"
-                    solve(board, row)
-                    board[row][col] == "."  
+                    row_list = list(board[row])
+                    row_list[col] = "Q"
+                    board[row] = "".join(row_list)
+                    solve(board, row+1)
+                    row_list[col] = "."
+                    board[row] = "".join(row_list)
 
         solve(board, 0)
-        return result   
+        return result                    
