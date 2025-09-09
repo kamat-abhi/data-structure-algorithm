c7b5244ae3de960c3b562c9462ab439ad552dc27
abhishek kamat <abhishekkamat142@gmail.com>
2025-09-09 09:37:43 +0530

    Implement N-Queens problem solution using backtracking

---
+++ recursion/N-Queens.py
@@ -0,0 +1,43 @@
+from typing import List
+
+#Approach 1: Backtracking
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+        board = ["." * n for _ in range(n)]
+
+        def isValid(board, row, col):
+            #upward check
+            i = row-1
+            while i>=0:
+                if board[row][col] == "Q":
+                    return False
+                i -= 1
+            #left diagonal upward check
+            i, j = row - 1, col - 1
+            while i>=0 and j>=0:
+                if board[row][col] == "Q":
+                    return False
+                i -= 1
+                j -= 1
+            #right diagonal upward check
+            i, j = row - 1, col + 1
+            while i>=0 and j<n:
+                if board[row][col] == "Q":
+                    return False
+                i -= 1
+                j += 1
+            return True
+
+        def solve(board, row):
+            if row == n:
+                result.append(board)
+                return
+            for col in range(n):
+                if isValid(board, row, col):
+                    board[row][col] == "Q"
+                    solve(board, row)
+                    board[row][col] == "."  
+
+        solve(board, 0)
+        return result   
